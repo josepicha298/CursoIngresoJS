423cf526fa34773fff322ba5d483d9a6cc3a15b0
josepicha298 <josepicha298@gmail.com>
2019-08-09 05:50:29 -0300

    9agosto

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,72 @@
 function mostrar()
 {
+    /*var seguir;
+    do
+    {
+        alert("hola que tal");
+        seguir=confirm("quiere continuar quiere que lo saludo de nuevo");
+
+    }while(seguir ==true)*/
+
+    /*var seguir;
+    do
+    {
+        alert("hola que tal");
+        
+    }while(confirm("quiere continuar quiere que lo saludo de nuevo"))
+    var letra;
+    var num = parseInt(prompt("Ingrese numero"));
+
+    for(i=-100; i<=100; i++)
+    {*/
+        //(!(letra>=65 && letra<=90) || (letra>= 97 && letra<=122))
+        var letra;
+        var num;
+        var contadorpar=0;
+        var contadorimpar=0;
+        var acumpos;
+        var acumneg;
+        var mayor;
+        var menor;
+        var mayorle;
+        var menorle;
+        var promedio=0;
+        do
+        {
+            letra= prompt("Ingrese datos");
+            num=parseInt(prompt("Ingrese numero entre -100 y 100:"));
+        }while(num < -100 || num>100 || isNaN(num)) 
+        {
+        num=parseInt(prompt("Error. Reingrese numero entre -100 y 100:"))
+        }
+        //me fijo la paridad
+        if(nem % 2 == 0)
+        {
+            contapar++;
+
+        }
+        else
+        {
+            contadorimpar++
+
+        }
+
+        if(num>0)
+        {
+            acumpos= acumpos + num;
+        }
+        if(nem>mayour || flag==0)
+        {
+            mayor = numero;
+            mayorle = letra;
+        }
+
+        if(num<menor)
+        {
+            menor=num;
+            menorle=letra
+        }
+
+    
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,11 +1,62 @@
 function mostrar()
 {
+
   var nota;
+  var sexo;
+  var acumuladornotas = 0;
+  var promedio;
+  var notabaja;
+  var sexobajo;
+  var contadorvaronesmayor=0;
+   
+
+  for(var i=0; i<5; i++)
+  {
+    nota = parseInt(prompt("Ingrese nota 0-10"));
+    //mintras no sea valida
+    //while(!(nota >=0 && nota <=10))
+    //mientras la nota sea invalida
+    while(nota < 0 || nota > 10 || isNaN(nota))
+    {
+      nota = prompt( "Error. Reinngrese su nota. entre a y 10")
+    }
+    //ya tengo la nota validada
+    sexo = prompt("Ingrese sexo:")//.toLowerCase();
+    //sexo=sexo.toLowerCase(); pasar a mayuscula a minuscula
+
+    //mientras es sexo no sea valido
+    while(!(sexo == "m" || sexo == "f" || sexo == "M" || sexo == "F"))
+    {
+      //mientras el sexo sea invalido
+      //while(sexo != "m" && sexo !="f")
+      sexo = prompt("Error.Reingrese sexo:")
+    }
+    //acumuladornotas=acumuladornotas + nota;
+    acumuladornotas += nota;
+    if(nota<notabaja || i==0)
+    {
+      notabaja = nota;
+      sexobajo = sexo;
+    }
+    if(sexo == "m" && nota >= 6)
+    {
+      contadorvaronesmayor++;
+    }
+    
+  }
+   promedio = acumuladornotas / 5;
+   alert("promedio: " + promedio);
+   document.write("promedio: "+promedio+"<br>");
+   document.write("notabaja: "+notabaja+ " sexo " +sexobajo +"<br>");
+   document.write("la cantidad de varones que tuvieron 6 o mas es: "+contadorvaronesmayor +"<br>");
+   
+
+
+  /*  var nota;
   var sexo;
   var promedio;
   var acumuladorNotas =0;
   var sexoBajo;
-  var sexoAlto;
   var varones6Mas = 0;
   var notaBaja = 10;
   
@@ -15,11 +66,11 @@ function mostrar()
     nota = parseInt(prompt("Ingrese su nota"));
     while ( nota <0 || nota >10)
     {
-      nota = prompt("Ingrese su nota. entre 0 y 10.")
+      nota = parseInt(prompt("Error. Reingrese su nota. entre 0 y 10."));
     }
       
     sexo = prompt("Ingrese su sexo");
-    while ( sexo != "f" || sexo != "m")
+    while ( sexo != "f" && sexo != "m")
     {
       sexo = prompt("Ingrese su sexo. f o m");
     } 
@@ -42,5 +93,5 @@ function mostrar()
 
    promedio = acumuladorNotas / 5;
 
-  document.write("El promedio de las notas totales es " + promedio + " La nota mas baja es " + notaBaja + " y el sexo de esa nota mas baja es " + sexoBajo + " y la cantidad de varones que tuvieron 6 o mas es " + varones6Mas); 
+  document.write("El promedio de las notas totales es " + promedio + " La nota mas baja es " + notaBaja + " y el sexo de esa nota mas baja es " + sexoBajo + " y la cantidad de varones que tuvieron 6 o mas es " + varones6Mas);*/
 }

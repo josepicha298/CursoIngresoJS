5ee4117dc0d8e7afe049b723e9d97c18a035119d
josepicha298 <52410388+josepicha298@users.noreply.github.com>
2020-02-05 11:55:28 -0300

    descuento

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,13 +4,16 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-    var Importe;
-    var Resultado;
+	var ingresoImporte;
+	var porcentaje=20;
+	var reSultado;
 
-    Importe=document.getElementById("importe").value;
-    Importe=parseInt(Importe);
-    
-    Resultado=Importe*25/100;
-    Resultado=Importe-Resultado;
-    document.getElementById("resultado").value=Resultado;    
+	ingresoImporte=parseInt(document.getElementById("importe").value);
+
+	reSultado=ingresoImporte*porcentaje/100;
+	reSultado=ingresoImporte-reSultado;
+
+	document.getElementById("resultado").value=reSultado;
+	
+	
 }

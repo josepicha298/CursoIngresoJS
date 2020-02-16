80269281a88a62a58209df45d5faaa34ba4fd7eb
josepicha298 <josepicha298@gmail.com>
2020-02-15 22:52:33 -0300

    cambio

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,29 +1,34 @@
 function mostrar()
 {
    //tomo la edad
-   /*var Edad;
-   var estadocivil;
-   Edad = document.getElementById("edad").value;
-   estadocivil = document.getElementById("estadoCivil").value;
-   if(Edad <=18)
-   {
-       if(estadocivil == "Casado")
-          alert("Es muy pequeño para NO ser soltero");       
-   }
-    
-   if(Edad <=18)
-   {
-       if(estadocivil == "Divorciado")
-          alert("Es muy pequeño para NO ser soltero");       
-   }*/
-
-   
-   
    var Edad;
    var estadocivil;
 
    Edad=document.getElementById("edad").value;
    estadocivil=document.getElementById("estadoCivil").value;
+
+
+   /*if(Edad <=18)
+    {
+        if(estadocivil == "Casado")
+          alert("Es muy pequeño para NO ser soltero");       
+    }
+      
+     if(Edad <=18)
+    {
+        if(estadocivil == "Divorciado")
+          alert("Es muy pequeño para NO ser soltero");       
+    }
+
+
+    if(Edad<18)
+    {
+      if(estadocivil!="Soltero")
+      {
+        alert("Es muy chico");
+      }
+    }*/
+
    
    if(Edad<18 && estadocivil!="Soltero")
    {

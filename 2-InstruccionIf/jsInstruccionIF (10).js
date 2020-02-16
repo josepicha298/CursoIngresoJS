7b077247afbe705287d1fc7b0a37e258476726b6
josepicha298 <josepicha298@gmail.com>
2020-02-16 16:03:58 -0300

    16/02

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -2,7 +2,7 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	var numero;
-	numero = Math.floor(Math.random() * 10) + 1
+	numero=Math.floor(Math.random()*11)+1;
 	
 	/*if(numero == 9 || numero ==10)
 	  {
@@ -17,19 +17,18 @@ function mostrar()
 		 alert("la proxima se puede "+numero);
 	 }*/
 
-	 if(numero >= 9 )
-	  {
-	     alert("EXCELENTE "+numero);
-      }
-	 else {
-	 if(numero >= 4) 
-	 {
-		 alert("APROBÓ "+numero);
-	 }
-	 else
-	 {
-		 alert("la proxima se puede "+numero);
-	 }
-	 }
+	if(numero>8)
+	{
+		alert("EXCELENTE "+numero);
+	}
+	else if(numero>4)
+	{
+		alert("APROBÓ "+numero);
+	} 
+	else
+	{
+		alert("Vamos, la proxima se puede "+numero);
+	}
+	
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -2,6 +2,6 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	var numero;
-	numero = Math.floor(Math.random() * 10) + 1
+	numero=Math.floor(Math.random()*11)+1;
 	alert(numero);
 }//FIN DE LA FUNCIÓN
